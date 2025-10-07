98e5d41b7ba20d9c8d6019295e3bb7f4553a62c4
Svajus000 <svajunaskoncius11@gmail.com>
2025-10-07 18:45:04 +0300

    Complete mobile-design

--- src/app/page.js
+++ src/app/page.js
@@ -8,44 +8,47 @@ export default function Home() {
   return (
     <>
       <main className="w-full min-h-screen bg-[#f2ebe3ff] flex justify-center items-center p-4">
-        <section className="max-w-[38rem] h-[29rem] bg-white rounded-lg overflow-hidden flex flex-col lg:flex-row ">
-          <div className="w-full lg:w-1/2 relative h-1/2 lg:h-full">
+        <section className="max-w-[38rem] bg-white rounded-lg overflow-hidden flex flex-col lg:flex-row">
+          <div className="relative w-full lg:w-1/2 h-128 lg:h-auto">
             <Image
               src="/images/image-product-desktop.jpg"
               alt="product_image"
               fill
-              className="object-cover hidden sm:block"
-            ></Image>
+              className="object-cover hidden lg:block"
+            />
             <Image
               src="/images/image-product-mobile.jpg"
               alt="product_image"
               fill
-              className="object-cover block sm:hidden"
-            ></Image>
+              className="object-cover block lg:hidden"
+            />
           </div>
-          <div className="p-8 w-full lg:w-1/2 h-1/2 lg:h-full">
+          <div className="p-8 w-full lg:w-1/2 flex flex-col justify-center">
             <h2
               className={`${montserrat.className} text-[1rem] pb-4 tracking-[0.2rem] uppercase`}
             >
               Perfume
             </h2>
             <h1
-              className={`${fraunces.className} text-[2rem] font-bold leading-none pb-6`}
+              className={`${fraunces.className} text-[2rem] font-bold leading-snug pb-6`}
             >
               Gabrielle Essence Eau De Parfum
             </h1>
-            <p className={`${montserrat.className} text-[1rem] pb-4`}>
-              {" "}
+            <p
+              className={`${montserrat.className} text-[1rem] pb-4 leading-relaxed`}
+            >
               A floral, solar and voluptuous interpretation composed by Olivier
               Polge, Perfumer-Creator for the House of CHANEL.
-            </p>{" "}
+            </p>
             <div className="flex items-center pb-6">
               <h1
                 className={`${fraunces.className} text-[2rem] text-[#3c8067ff] font-semibold pr-6`}
               >
                 $149.99
               </h1>
-              <h3 className={`${fraunces.className} text-[1rem]`}>$169.99</h3>
+              <h3 className={`${fraunces.className} text-[1rem] line-through`}>
+                $169.99
+              </h3>
             </div>
             <a
               className={`${montserrat.className} block w-full h-[2.5rem] bg-[#3c8067ff] flex justify-center items-center rounded-lg text-white text-[12px] hover:bg-[#1b4133ff] cursor-pointer`}
@@ -56,7 +59,7 @@ export default function Home() {
                 width={14}
                 height={14}
                 className="mr-2"
-              ></Image>
+              />
               Add to Cart
             </a>
           </div>
