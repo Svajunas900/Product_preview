84958b934f0174aebab5df0c6dea9aa72142b5e4
Svajus000 <svajunaskoncius11@gmail.com>
2025-10-07 18:25:43 +0300

    Complete desktop-design

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,67 @@
 import Image from "next/image";
+import { Montserrat, Fraunces } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["500", "700"] });
+const fraunces = Fraunces({ subsets: ["latin"], weight: ["700"] });
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <>
+      <main className="w-full min-h-screen bg-[#f2ebe3ff] flex justify-center items-center p-4">
+        <section className="max-w-[38rem] h-[29rem] bg-white rounded-lg overflow-hidden flex flex-col lg:flex-row ">
+          <div className="w-full lg:w-1/2 relative h-1/2 lg:h-full">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+              src="/images/image-product-desktop.jpg"
+              alt="product_image"
+              fill
+              className="object-cover hidden sm:block"
+            ></Image>
+            <Image
+              src="/images/image-product-mobile.jpg"
+              alt="product_image"
+              fill
+              className="object-cover block sm:hidden"
+            ></Image>
+          </div>
+          <div className="p-8 w-full lg:w-1/2 h-1/2 lg:h-full">
+            <h2
+              className={`${montserrat.className} text-[1rem] pb-4 tracking-[0.2rem] uppercase`}
+            >
+              Perfume
+            </h2>
+            <h1
+              className={`${fraunces.className} text-[2rem] font-bold leading-none pb-6`}
+            >
+              Gabrielle Essence Eau De Parfum
+            </h1>
+            <p className={`${montserrat.className} text-[1rem] pb-4`}>
+              {" "}
+              A floral, solar and voluptuous interpretation composed by Olivier
+              Polge, Perfumer-Creator for the House of CHANEL.
+            </p>{" "}
+            <div className="flex items-center pb-6">
+              <h1
+                className={`${fraunces.className} text-[2rem] text-[#3c8067ff] font-semibold pr-6`}
+              >
+                $149.99
+              </h1>
+              <h3 className={`${fraunces.className} text-[1rem]`}>$169.99</h3>
+            </div>
+            <a
+              className={`${montserrat.className} block w-full h-[2.5rem] bg-[#3c8067ff] flex justify-center items-center rounded-lg text-white text-[12px] hover:bg-[#1b4133ff] cursor-pointer`}
+            >
+              <Image
+                src="/images/icon-cart.svg"
+                alt="cart"
+                width={14}
+                height={14}
+                className="mr-2"
+              ></Image>
+              Add to Cart
+            </a>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
